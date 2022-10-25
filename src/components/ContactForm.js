34a6c05d8d1318b470d93ea23ff7223ebda8b6b0
Manissha <manissha52@yahoo.dk>
2022-10-25 08:22:50 +0200

    added required to validate

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,65 @@
+let ContactForm = function () {
+  let element = document.createElement("form");
+  element.classList.add("contactForm");
 
-
-let ContactForm = function() {
-
-    let element = document.createElement('form')
-    element.classList.add('contactForm')
-    
-    element.innerHTML = `
+  element.innerHTML = `
     <div className="contactForm_group">
         <!-- for og id hører sammen her, når man klikker  -->
         <label for='firstname'>Fornavn:</label>
-        <input type ="text" name="firstname" id="firstname"/>
+        <input type ="text" name="firstname" id="firstname" required minlength=2/>
     </div>
-        
-    `
-    
-    return element
+    <div class="contactForm_group">
+        <label for="lastname">Efternavn:</label>
+        <input type="text" name="lastname" id="lastname" required minlength=3 />
+    </div>
+    <div class="contactForm__group">
+        <label for="address">Adresse:</label>
+        <input type="text" name="address" id="address" />
+    </div>
+    <div class="contactForm__group">
+        <label for="postalcode">Post nr.:</label>
+        <input type="text" name="postalcode" id="postalcode" />
+    </div>
+    <div class="contactForm__group">
+        <label for="city">By:</label>
+        <input type="text" name="city" id="city" />
+    </div>
+    <div class="contactForm__group">
+        <label for="email">E-mail:</label>
+        <!-- regex email -->
+        <input type="email" name="email" id="email" required pattern="^[^\s@]+@[^\s@]+\.[^\s@]{2,}$"/>
+    </div>
+    <div class="contactForm__group">
+        <label for="phone">Telefon:</label>
+        <input type="tel" name="phone" id="phone" />
+    </div>
+    <div class="contactForm__group">
+        <label for="message">Besked:</label>
+        <textarea name="message" id="message" cols="30" rows="10" required minlength=10></textarea>
+    </div>
+    <button type="submit">Send!</button>
+    `;
+
+  element.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // console.log(e.target.firstname.value);
+    if(e.target.firstname.value === ""){
+        console.log
     }
-    
-    export default ContactForm
+    let data = {
+        firstname: e.target.firstname.value,
+        lastname: e.target.lastname.value,
+        address: e.target.address.value,
+        postalcode: e.target.postalcode.value,
+        city: e.target.city.value,
+        email: e.target.email.value,
+        phone: e.target.phone.value,
+        message: e.target.message.value,
+    }
+    console.log(data)
+
+  });
+  return element;
+};
+
+export default ContactForm;

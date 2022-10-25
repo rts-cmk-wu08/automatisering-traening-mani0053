4c59651b29d25467629419a817830c95e7d168c9
Manissha <manissha52@yahoo.dk>
2022-10-25 09:42:56 +0200

    added required validate number

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,29 +14,30 @@ let ContactForm = function () {
     </div>
     <div class="contactForm__group">
         <label for="address">Adresse:</label>
-        <input type="text" name="address" id="address" />
+        <input type="text" name="address" id="address" required minlength=4/>
     </div>
     <div class="contactForm__group">
         <label for="postalcode">Post nr.:</label>
-        <input type="text" name="postalcode" id="postalcode" />
+        <input title="Indtast et tal." type="text" step="100" min="0" max="1000" name="postalcode" id="postalcode" required pattern="[0-9]{4}"/>
     </div>
     <div class="contactForm__group">
         <label for="city">By:</label>
-        <input type="text" name="city" id="city" />
+        <input type="text" name="city" id="city" required minlength=4 />
     </div>
     <div class="contactForm__group">
         <label for="email">E-mail:</label>
         <!-- regex email -->
-        <input type="email" name="email" id="email" required pattern="^[^\s@]+@[^\s@]+\.[^\s@]{2,}$"/>
+        <input title="Indtast en valid email adresse." type="email" name="email" id="email" required pattern="[A-Za-z0-9-._+]+@[A-Za-z0-9.-]+\[.]+[a-zA-Z]{2,}"/>
     </div>
     <div class="contactForm__group">
         <label for="phone">Telefon:</label>
-        <input type="tel" name="phone" id="phone" />
+        <input title="Indtast et tal." type="tel" name="phone" id="phone" required pattern="[0-9]{8}"/>
     </div>
     <div class="contactForm__group">
         <label for="message">Besked:</label>
         <textarea name="message" id="message" cols="30" rows="10" required minlength=10></textarea>
     </div>
+    
     <button type="submit">Send!</button>
     `;
 
